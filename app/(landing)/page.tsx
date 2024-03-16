37258c1025572771ae955e1736f1aaeb6b38f8d5
Rajat Kumar <rajat9258@gmail.com>
2024-03-17 00:48:17 +0530

    clerk setup

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      This is landing page (unprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button>Log in</Button>
+        </Link>
+      </div>
+      <div>
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
