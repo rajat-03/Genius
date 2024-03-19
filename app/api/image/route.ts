c57db331293df93cc4c87ab7eb77f59a59ac32b9
Rajat Kumar <rajat9258@gmail.com>
2024-03-19 19:49:16 +0530

    Add image generation page

---
+++ app/api/image/route.ts
@@ -0,0 +1,50 @@
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+
+export async function POST(req: Request) {
+    try {
+        const { userId } = auth();
+        const body = await req.json();
+        const { prompt, amount = 1, resolution = "512x512" } = body;
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 })
+        }
+
+        if (!openai) {
+            return new NextResponse("OpenAI API Key not configured", { status: 500 })
+        }
+
+        if(!prompt)
+        {
+            return new NextResponse("Prompt is required", {status: 400})
+        }
+
+        if(!amount)
+        {
+            return new NextResponse("Amount is required", {status: 400})
+        }
+
+        if(!resolution)
+        {
+            return new NextResponse("Resolution is required", {status: 400})
+        }
+
+        const chatCompletion = await openai.images.generate({
+            prompt,
+            n: parseInt(amount,10),
+            size: resolution,
+        });
+
+        return NextResponse.json(chatCompletion.data)
+    } catch (error) {
+        console.log("[IMAGE_GENERATION_ERROR]", error)
+        return new NextResponse("Internal error", { status: 500 })
+    }
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                protocol:"https",
+                hostname:"oaidalleapiprodscus.blob.core.windows.net",
+                port:"",
+                pathname:"**",
+            }
+        ]
+    }
+};
 
 export default nextConfig;
